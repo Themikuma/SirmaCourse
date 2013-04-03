b8dd76c58f6572755b11fa02485efb5ef88b619d
Георги Димитров <Themikuma@gmail.com>
2013-04-03 15:15:04 +0300

    Четвърта и пета задача
    За четвърта задача само променям пътищата на файловете, а по пета давам
    вече готовото дърво с тестове и документация.

--- Objects in java/src/main/java/BinaryHeterogeneousTree.java
+++ Objects in java/src/main/java/BinaryHeterogeneousTree.java
@@ -1,8 +1,9 @@
 package main.java;
 
 /**
- * A realization of the binary search tree. It has implemented methods for inserting, printing all
- * the elements sorted and searching for a given element
+ * A realization of the binary search tree for all objects. It has implemented methods for
+ * inserting, printing all the elements sorting them by their hash codes and searching for a given
+ * element
  * 
  * @author gdimitrov
  */
@@ -12,7 +13,7 @@ public class BinaryHeterogeneousTree {
 	/**
 	 * This class implements a single node in the tree
 	 */
-	private class BinaryHeterogeneousTreeNode {
+	private class BinaryHeterogeneousTreeNode implements Comparable<Object> {
 		private Object value;
 		private BinaryHeterogeneousTreeNode leftChild;
 		private BinaryHeterogeneousTreeNode rightChild;
@@ -21,6 +22,17 @@ public class BinaryHeterogeneousTree {
 			this.value = value;
 			leftChild = null;
 			rightChild = null;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			if (System.identityHashCode(this.value) > System.identityHashCode(o)) {
+				return 1;
+			}
+			if (System.identityHashCode(this.value) < System.identityHashCode(o)) {
+				return -1;
+			}
+			return 0;
 		}
 	}
 
@@ -51,9 +63,9 @@ public class BinaryHeterogeneousTree {
 		if (currentNode == null) {
 			currentNode = new BinaryHeterogeneousTreeNode(value);
 		} else {
-			if (value.hashCode() > currentNode.value.hashCode()) {
+			if (currentNode.compareTo(value) == -1) {
 				currentNode.rightChild = insert(value, currentNode.rightChild);
-			} else if (value.hashCode() < currentNode.value.hashCode()) {
+			} else if (currentNode.compareTo(value) == 1) {
 				currentNode.leftChild = insert(value, currentNode.leftChild);
 			} else {
 				throw new IllegalArgumentException("Duplicate items");
@@ -63,31 +75,54 @@ public class BinaryHeterogeneousTree {
 	}
 
 	/**
-	 * Sorts the tree from lowest to highest.
+	 * Prints the objects in the tree sorted by hash code from lowest to highest.
 	 * 
 	 * @return the tree as a sorted string
 	 */
 	public String printTree() {
-		return sortTree(this.root);
+		return printTree(this.root);
 	}
 
 	/**
-	 * The actual implementation of the sortTree function
+	 * The actual implementation of the printTree function
 	 * 
 	 * @param currentNode
 	 *            - the current node that is being checked
 	 * @return the sorted string so far.
 	 */
-	private String sortTree(BinaryHeterogeneousTreeNode currentNode) {
+	private String printTree(BinaryHeterogeneousTreeNode currentNode) {
 		StringBuilder builder = new StringBuilder();
 		if (currentNode.leftChild != null) {
-			builder.append(sortTree(currentNode.leftChild));
+			builder.append(printTree(currentNode.leftChild));
 		}
 		builder.append(currentNode.value + " ");
 		if (currentNode.rightChild != null) {
-			builder.append(sortTree(currentNode.rightChild));
+			builder.append(printTree(currentNode.rightChild));
 		}
 		return builder.toString();
+	}
+
+	public boolean search(Object value) {
+		return search(this.root, value);
+	}
+
+	/**
+	 * @param currentNode
+	 *            - the node that is being checked in this call
+	 * @param value
+	 *            - the value that's being searched for
+	 * @return True if the value was found.
+	 */
+	private boolean search(BinaryHeterogeneousTreeNode currentNode, Object value) {
+		if (currentNode.compareTo(value) == 0) {
+			return true;
+		}
+		if ((currentNode.compareTo(value) == 1) && (currentNode.leftChild != null)) {
+			return search(currentNode.leftChild, value);
+		} else if ((currentNode.compareTo(value) == -1) && (currentNode.rightChild != null)) {
+			return search(currentNode.rightChild, value);
+		}
+		return false;
 	}
 
 }
